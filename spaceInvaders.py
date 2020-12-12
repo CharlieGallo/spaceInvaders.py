225205a82644c9c10b0fe278a7ad82fbb6c31fcb
Charlie Galloway <charlottegalloway90@gmail.com>
2020-12-12 20:10:40 +0000

    Project complete

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -1,6 +1,7 @@
 import sys, pygame
 import random
 import math
+from pygame import mixer
 
 # Initialise the pygame
 pygame.init()
@@ -12,6 +13,10 @@ screen = pygame.display.set_mode((800, 600))
 
 # Background
 background = pygame.image.load('background.png')
+
+#Background Music
+mixer.music.load('background.wav')
+mixer.music.play(-1) #-1 means the sound will play on a loop
 
 # Title and icon
 pygame.display.set_caption("Space Invaders")
@@ -25,11 +30,20 @@ playerY = 480
 playerX_change = 0  # create variable to use in event for loop
 
 # Enemy
-enemyImg = pygame.image.load('enemy.png')  # 64 pixels
-enemyX = random.randint(0, 736)  # Enemy randomly placed on screen
-enemyY = random.randint(50, 150)
-enemyX_change = 3
-enemyY_change = 40
+# Use lists to create multiple enemies
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))  # 64 pixels
+    enemyX.append(random.randint(0, 735))  # Enemy randomly placed on screen
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(3)
+    enemyY_change.append(40)
 
 # Bullet
 # Ready - You can't see the bullet on the screen
@@ -41,7 +55,25 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
-score = 0
+# Score on screen
+score_value = 0
+font = pygame.font.Font('darkhornet.ttf', 32)
+textX = 10
+textY = 10
+
+# Game Over text
+game_over_font = pygame.font.Font('darkhornet.ttf', 64)
+
+
+# Display score on screen using render() and then blit()
+# String value, set True to display in screen, (colour of font)
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def game_over_text():
+    game_over_text = game_over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(game_over_text, (250, 250))
 
 # Function to display and move player image at set X, Y co-ordinates
 # blit() method draws image - assigns pixels to X, Y pos.
@@ -50,8 +82,8 @@ def player(x, y):
 
 
 # Function to display and move enemy
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 
 def fire_bullet(x, y):
@@ -71,7 +103,7 @@ def is_collision(enemyX, enemyY, bulletX, bulletY):
         return False
 
 
-# game loop
+# game loop - keeps displaying screen and playing images
 running = True
 while running:
 
@@ -101,7 +133,10 @@ while running:
                 # print('Right arrow')
                 playerX_change = 4
             if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
+                if bullet_state == "ready":
+                    # Plays bullet sound
+                    bullet_sound = mixer.Sound('laser.wav')
+                    bullet_sound.play()
                     # Gets current X co-ord of spaceship
                     bulletX = playerX
                     fire_bullet(bulletX, bulletY)
@@ -122,37 +157,56 @@ while running:
         playerX = 736
 
     ##ENEMY MOVEMENT
-    # Change X co-ordinate passed to enemy() function
-    enemyX += enemyX_change
-    # When enemy meets boundary of screen it goes back in opposite direction
-    if enemyX <= 0:
-        enemyX_change = 3
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -3
-        enemyY += enemyY_change
+    # For loop to handle multiple enemies
+    for i in range(num_of_enemies):
+
+        #Game Over
+        # If enemy hits the ship then all the enemies will be removed from the screen
+        # and the game_over_text function is called
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+
+        # Change X co-ordinate passed to enemy() function
+        enemyX[i] += enemyX_change[i]
+        # When enemy meets boundary of screen it goes back in opposite direction
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 3
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -3
+            enemyY[i] += enemyY_change[i]
+
+        # COLLISION
+        collision = is_collision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            # Plays explosion sound when enemy is destroyed
+            explosion_sound = mixer.Sound('explosion.wav')
+            explosion_sound.play()
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        # Call enemy() function to display player image on screen
+        enemy(enemyX[i], enemyY[i], i)
 
     ##BULLET MOVEMENT
     if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
 
-    if bullet_state is "fire":
+    if bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
-    #COLLISION - must come after the enemy and bullet co-ords are set
-    collision = is_collision(enemyX, enemyY, bulletX, bulletY)
-    if collision:
-        bulletY = 480
-        bullet_state = "ready"
-        score += 1
-        print(score)
-
     # Call player() function to display player image on screen
     player(playerX, playerY)
-    # Call enemy() function to display player image on screen
-    enemy(enemyX, enemyY)
+
+    # Call show_score function to display on screen
+    show_score(textX, textY)
 
     # Update changes to the display
     pygame.display.update()
